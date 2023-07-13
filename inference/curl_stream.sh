1b54e7406b6ce9316441e92c7a53b0b675b8f305
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2023-07-14 00:31:14 +0800

    shift to code350

--- inference/curl_stream.sh
+++ inference/curl_stream.sh
@@ -1,3 +1,3 @@
 curl --location '127.0.0.1:8080/generate_stream' \
 --header 'Content-Type: application/json' \
---data '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":17}}'
+--data '{"inputs":"def serve():","parameters":{"max_new_tokens":17}}'
--- inference/runtgi.sh
+++ inference/runtgi.sh
@@ -1,5 +1,5 @@
 rm -rf data
-export model=bigscience/bloom-560m
+export model=Salesforce/codegen-350M-multi
 export num_shard=1
 export volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
 #docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:latest --model-id $model --num-shard $num_shard
