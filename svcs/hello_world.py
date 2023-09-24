6f1a039fe421ea913559cca8cc49a4bfe3512543
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2023-09-24 18:26:50 +0800

    keep running the svc

--- svcs/hello_world.py
+++ svcs/hello_world.py
@@ -1,12 +1,14 @@
+import ray
+ray.init(dashboard_host="0.0.0.0")
 import requests
 from starlette.requests import Request
 from typing import Dict
-import ray
+
 from ray import serve
 
 
 # 1: Define a Ray Serve application.
-@serve.deployment(route_prefix="/")
+@serve.deployment
 class MyModelDeployment:
     def __init__(self, msg: str):
         # Initialize model state: could be very large neural net weights.
@@ -17,11 +19,14 @@ class MyModelDeployment:
 
 
 app = MyModelDeployment.bind(msg="Hello world!")
-
-# 2: Deploy the application locally.
-ray.init(dashboard_host="0.0.0.0")
 serve.run(app, host="0.0.0.0")
+#print(requests.get("http://127.0.0.1:8000/").json())
 
-# # 3: Query the application and print the result.
-# print(requests.get("http://localhost:8000/").json())
-# # {'result': 'Hello world!'
+import time
+while True:
+    time.sleep(5)
+    print(serve.list_deployments())
+
+# # # # 3: Query the application and print the result.
+
+# # {'result': 'Hello world!'}
