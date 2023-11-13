769776dd5b2e00938ff84a7763279060345680d4
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2023-11-13 22:50:53 +0800

    check env preexisiting

--- startup.sh
+++ startup.sh
@@ -5,11 +5,19 @@ mkdir -p /scratchpad/data/conda_envs
 #mkdir -p /scratchpad/data/conda_envs/pkgs
 
 conda config --prepend envs_dirs /scratchpad/data/conda_envs
-if [[ $(conda env list | awk '{print $1}' | grep -w 'ray_env') != 'ray_env' ]]; then
+ls -lah /scratchpad/data/conda_envs
+conda env list
+
+#if [[ $(conda env list | awk '{print $1}' | grep -w 'text-generation-inference') == 'text-generation-inference' ]]; 
+export DIRECTORY="/scratchpad/data/conda_envs/ray_env"
+if [ -d "$DIRECTORY" ]; then
+    echo "ray_env" already exists
+else
     chmod +x /scratchpad/installers/create_ray_env.sh && source /scratchpad/installers/create_ray_env.sh
 fi
 
 source activate ray_env
+
 ray start --head
 jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password='' -y & jupnb=$!
 /usr/local/bin/codeserver & code=$!
